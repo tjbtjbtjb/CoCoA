84ccb96c7e00083aa4a6545feddc6e7d27342d44
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2020-06-10 11:53:36 +0200

    good indent, adding test notebook

--- cocoa/covid19.py
+++ cocoa/covid19.py
@@ -2,7 +2,7 @@
 
 """ 
 Project : CoCoA
-Date :    april/may 2020
+Date :    april-june 2020
 Authors : Olivier Dadoun, Julien Browaeys, Tristan Beau
 Copyright © CoCoa-team-17
 License: See joint LICENSE file
@@ -20,178 +20,166 @@ import pandas
 from collections import defaultdict
 import numpy as np
 from datetime import datetime as dt
+import pandas as pd
 
-#class EpidemiologicalData:
-#	# empty parent class
-#	def __init__(self):
-#		# do nothing
 
-class JHUCSSEdata(): 
-	def __init__(self):
-		self.__baseUrl="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/"
-		self.__baseUrl+="csse_covid_19_data/csse_covid_19_time_series/"
-		self.whichDataList=["deaths","confirmed","recovered"] 
-		self.__pandasData={}
+class JHUCSSEdata():
+    def __init__(self, **kwargs):
+        self.__baseUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/"
+        self.__baseUrl += "csse_covid_19_data/csse_covid_19_time_series/"
+        self.whichDataList = ["deaths", "confirmed", "recovered"]
+        self.__pandasData = {}
 
-		for w in  self.whichDataList:
-			fileName="time_series_covid19_" + w + "_global.csv"
-			url=self.__baseUrl+fileName
-			self.__pandasData[w] = pandas.read_csv(url)
+        aphpdata = kwargs.get('aphpdata', None)
+        if aphpdata:
+            self.__pandasData = self.convertAPHP()
+        else:
+            for w in self.whichDataList:
+                fileName = "time_series_covid19_" + w + "_global.csv"
+                url = self.__baseUrl+fileName
+                self.__pandasData[w] = pandas.read_csv(url)
 
-	def getBaseUrl(self):
-		return self.__baseUrl
+    def getBaseUrl(self):
+        return self.__baseUrl
 
-	def getRawData(self):
-		return self.__pandasData
+    def getRawData(self):
+        return self.__pandasData
 
-class UsefullFunction():
-  def __init__(self):
-    pass
-
-  def flat_list(self,matrix):
-    flatten_matrix=[]
-    for sublist in matrix: 
-        for val in sublist: 
-            flatten_matrix.append(val)
-    return flatten_matrix
-
-  def str2bool(self,v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')   
-
-class Parser():
-    def __init__(self,d=0):
-      
-      if not d:
-      	d=JHUCSSEdata()
-
-      self.dates={}
-      self.dicos_countries={}
-      self.dict_sum_data={}
-      self.total_current_cases={}
-      self.masked_points={}
-      self.diff_days={}
-    
-      self.which_data_list=d.whichDataList 
-
-      df=d.getRawData()
-    
-      #nb_notfound=0
-      for w in self.which_data_list:
-        self.dicos_countries[w]=defaultdict(list)
-        self.dates[w]  = list(df[w].head(0))[4:]
-      
-        for index, row in df[w].iterrows():
-          country=row["Country/Region"]
-          #if country in list(Population_Tab["Country (or dependency)"]) :
-          value=[int(i) if i is not '' else -1 for i in \
-                   row[self.dates[w]].values]
-          self.dicos_countries[w][country].append(value)
-          #else:
-            #print(' ===>>> ',country,' not found')
-          #  nb_notfound+=1
-      
-        self.dict_sum_data[w]=defaultdict(list)
-        self.total_current_cases[w]=defaultdict(list)
-        i_start = 0
-
-        self.masked_points[w]=defaultdict(list)
-        self.diff_days[w]=defaultdict(list)
-
-        for keys in self.dicos_countries[w]:
-          # Using list comprehension
-          res = [sum(i) for i in zip(*self.dicos_countries[w][keys])] 
-          self.dict_sum_data[w][keys].append(res)   
         
-          self.dict_sum_data[w][keys]=\
-              UsefullFunction().flat_list(self.dict_sum_data[w][keys])
-          #masked non existing value , it could happen ...
-          self.masked_points[w][keys]=\
-              np.ma.array(self.dict_sum_data[w][keys][i_start:])
-          self.diff_days[w][keys]=[j-i for i, j in zip(self.masked_points[w][keys][:-1],\
-                                                     self.masked_points[w][keys][1:])] 
-          self.diff_days[w][keys].insert(0,0)
-          self.diff_days[w][keys]=np.array(self.diff_days[w][keys])
+class Parser():
+    def __init__(self, d=0):
+
+        if not d:
+            d = JHUCSSEdata()
+
+        self.dates = {}
+        self.dicos_countries = {}
+        self.dict_sum_data = {}
+        self.total_current_cases = {}
+        self.masked_points = {}
+        self.diff_days = {}
+
+        self.which_data_list = d.whichDataList
+
+        df = d.getRawData()
+
+        for w in self.which_data_list:
+            self.dicos_countries[w] = defaultdict(list)
+
+            self.dates[w] = list(df[w].head(0))[4:]
+
+            for index, row in df[w].iterrows():
+                country = row["Country/Region"]
+                # if country in list(Population_Tab["Country (or dependency)"]) :
+                value = [int(i) if i != '' else -1 for i in
+                         row[self.dates[w]].values]
+                self.dicos_countries[w][country].append(value)
+                # else:
+                #print(' ===>>> ',country,' not found')
+                #  nb_notfound+=1
+
+            self.dict_sum_data[w] = defaultdict(list)
+            self.total_current_cases[w] = defaultdict(list)
+
+            self.masked_points[w] = defaultdict(list)
+            self.diff_days[w] = defaultdict(list)
+
+            for keys in self.dicos_countries[w]:
+                # Using list comprehension
+                res = [sum(i) for i in zip(*self.dicos_countries[w][keys])]
+                self.dict_sum_data[w][keys].append(res)
+                self.dict_sum_data[w][keys] =\
+                    self.flat_list(self.dict_sum_data[w][keys])
+                # masked non existing value , it could happen ...
+                self.masked_points[w][keys] =\
+                    np.ma.array(self.dict_sum_data[w][keys])
+
+                self.diff_days[w][keys] = [j-i for i, j in zip(self.masked_points[w][keys][:-1],
+                                                               self.masked_points[w][keys][1:])]
+
+                self.diff_days[w][keys].insert(0, 0)
+                self.diff_days[w][keys] = np.array(self.diff_days[w][keys])
+
+    def flat_list(self, matrix):
+        flatten_matrix = []
+        for sublist in matrix:
+            for val in sublist:
+                flatten_matrix.append(val)
+        return flatten_matrix
 
     def getMaskedPoint(self):
         return self.masked_points
-    
+
     def getDiffDays(self):
         return self.diff_days
 
-    def getStats(self,**kwargs):
-    	if not isinstance(kwargs['country'],list):
-    		clist=[kwargs['country']]
-    	else:
-    		clist=kwargs['country']
+    def getStats(self, **kwargs):
+        if not isinstance(kwargs['country'], list):
+            clist = [kwargs['country']]
+        else:
+            clist = kwargs['country']
 
-    	clist=[cName.replace('Czech Republic (Czechia)','Czechia') for cName in clist]
+        clist = [cName.replace('Czech Republic (Czechia)', 'Czechia')
+                 for cName in clist]
 
-    	diffout=np.array(tuple(dict((c,self.getDiffDays()[kwargs['which']][c]) for c in clist).values()))
-    	sumout=np.array(tuple(dict((c,self.getMaskedPoint()[kwargs['which']][c].data) for c in clist).values()))
+        diffout = np.array(
+            tuple(dict((c, self.getDiffDays()[kwargs['which']][c]) for c in clist).values()))
+        sumout = np.array(tuple(dict(
+            (c, self.getMaskedPoint()[kwargs['which']][c].data) for c in clist).values()))
 
-    	option=kwargs.get('option', None)
-    	if option == 'nonneg':
-    		diffout=np.array(diffout,dtype=float)
-    		sumout=np.array(sumout,dtype=float)
-    		for c in range(diffout.shape[0]):
-    			yy=np.array(diffout[c,:],dtype=float)
-    			for kk in np.where(yy<0)[0]:
-    				k=int(kk)
-    				val_to_repart = -yy[k]
-    				if k < np.size(yy)-1:
-    					yy[k]=(yy[k+1]+yy[k-1])/2
-    				else:
-    					yy[k]=yy[k-1]
-    				val_to_repart = val_to_repart + yy[k]
-    				s=np.sum(yy[0:k])
-    				yy[0:k]=yy[0:k]*(1-float(val_to_repart)/s)
+        option = kwargs.get('option', None)
+        if option == 'nonneg':
+            diffout = np.array(diffout, dtype=float)
+            sumout = np.array(sumout, dtype=float)
+            for c in range(diffout.shape[0]):
+                yy = np.array(diffout[c, :], dtype=float)
+                for kk in np.where(yy < 0)[0]:
+                    k = int(kk)
+                    val_to_repart = -yy[k]
+                    if k < np.size(yy)-1:
+                        yy[k] = (yy[k+1]+yy[k-1])/2
+                    else:
+                        yy[k] = yy[k-1]
 
-    			diffout[c,:]=yy
-    			sumout[c,:]=np.cumsum(yy)
+                    val_to_repart = val_to_repart + yy[k]
+                    s = np.sum(yy[0:k])
+                    yy[0:k] = yy[0:k]*(1-float(val_to_repart)/s)
+                diffout[c, :] = yy
+                sumout[c, :] = np.cumsum(yy)
 
-    	if kwargs['type']=='Cumul':
-    		out = sumout
-    	elif kwargs['type']=='Diff':
-    		out = diffout
-    	else:
-    		raise TypeError("Invalid keyword type argument %s , waiting for Cumul or Diff." % key)    
+        output = kwargs.get('output',None)
 
-    	if out.shape[0] == 1:
-    		return out[0]
-    	else:
-    		return out.T
+        if kwargs['type'] == 'Cumul':
+            out = sumout
+        elif kwargs['type'] == 'Diff':
+            out = diffout
+        else:
+            raise TypeError(
+                "Invalid keyword type argument %s , waiting for Cumul or Diff." % key)
 
-    def getPandasStats(self,**kwargs):
-      #return diff pandas - from diff it is simple to have cumul -
-      if not isinstance(kwargs['country'],list):
-        clist=[kwargs['country']]
-      else:
-        clist=kwargs['country']
-        
-      clist=[cName.replace('Czech Republic (Czechia)','Czechia') for cName in clist]
-      d=[]
-      for coun in clist:
-        i=0
-        for datos in self.getDates():
-          d.append(
-                   {
-                    'country': coun,
-                    'date'   : dt.strptime(datos,'%m/%d/%y'),
-                    'cases'  : self.getDiffDays()[kwargs['which']][coun][i]
-                   }
-         )
-          i+=1  
-      babypandas=pandas.DataFrame(d)
-      return babypandas
+        if output == "pandas":
+            d = []
+            for enum, coun in enumerate(clist):
+                i = 0
+                for datos in self.getDates():
+                    d.append(
+                        {
+                            'country': coun,
+                            'date': dt.strptime(datos, '%m/%d/%y'),
+                            'cases': out[enum, :][i]
+                        }
+                    )
+                    i += 1
+            babypandas = pd.DataFrame(d)
+            return babypandas
+        else:
+            if out.shape[0] == 1:
+                return out[0]
+            else:
+                return out.T
 
     def getCountries(self):
-    	return np.array(tuple(self.getMaskedPoint()[self.which_data_list[0]].keys()))
+        return np.array(tuple(self.getMaskedPoint()[self.which_data_list[0]].keys()))
 
-    
     def getDates(self):
-    	return np.array(self.dates[self.which_data_list[0]])
+        return np.array(self.dates[self.which_data_list[0]])
--- cocoa/world.py
+++ cocoa/world.py
@@ -2,7 +2,7 @@
 
 """ 
 Project : CoCoA
-Date :    april/may 2020
+Date :    april-june 2020
 Authors : Olivier Dadoun, Julien Browaeys, Tristan Beau
 Copyright © CoCoa-team-17
 License: See joint LICENSE file
@@ -12,30 +12,76 @@ About :
 
 Provide simple quantitative information about countries in the worlds for normalisation purpuses of the CoCoA project.
 The database in use is explicitly available throuh the getBaseUrl() method.
-The European country list has been added for CoCoa specific needs.
+The EU and all continental country list have been added for CoCoa specific needs.
 
 """
-
+            
 import requests
 import pandas
 
+
 class WorldInfo:
-	__pandasData=pandas.DataFrame()
+    __pandasData = pandas.DataFrame()
 
-	def __init__(self):
-		self.__url="https://www.worldometers.info/world-population/population-by-country/"
-		htmlContent=requests.get(self.__url).content
-		self.__pandasData=pandas.read_html(htmlContent)[0][['Country (or dependency)','Population (2020)','Land Area (Km²)']]
-		self.__pandasData.columns=['Country','Population','Area']
+    def __init__(self):
+        self.__url = "https://www.worldometers.info/world-population/population-by-country/"
+        htmlContent = requests.get(self.__url).content
+        self.__pandasData = pandas.read_html(htmlContent)[
+            0][['Country (or dependency)', 'Population (2020)', 'Land Area (Km²)']]
+        self.__pandasData.columns = ['Country', 'Population', 'Area']
 
-	def getBaseUrl(self):
-		return self.__url
+    def getBaseUrl(self):
+        return self.__url
 
-	def getData(self):
-		return self.__pandasData
+    def getData(self):
+        return self.__pandasData
 
-	def getEuropeanCountries(self):
-		return ['Portugal','Spain','Ireland','United Kingdom','France','Italy','Germany','Belgium', \
-			'Netherlands','Luxembourg','Austria','Denmark','Sweden','Finland', \
-			'Estonia','Latvia','Lithuania','Czech Republic (Czechia)','Poland','Slovakia','Hungary','Slovenia', \
-			'Croatia','Romania','Bulgaria','Greece','Malta','Cyprus']
+    def getEUCountries(self):
+        country = ['Portugal', 'Spain', 'Ireland', 'United Kingdom', 'France', 'Italy', 'Germany', 'Belgium',
+                   'Netherlands', 'Luxembourg', 'Austria', 'Denmark', 'Sweden', 'Finland',
+                   'Estonia', 'Latvia', 'Lithuania', 'Poland', 'Slovakia', 'Hungary', 'Slovenia',
+                   'Croatia', 'Romania', 'Bulgaria', 'Greece', 'Malta', 'Cyprus']
+        return sorted(country)
+
+    def getEuropeCountries(self):
+        country = ['Andorra', 'Albania', 'Austria', 'Belgium', 'Bulgaria', 'Belarus',
+                   'Germany', 'Denmark', 'Estonia', 'Finland', 'France', 'Greece', 'Hungary',
+                   'Ireland', 'Iceland', 'Italy', 'Liechtenstein', 'Lithuania',
+                   'Luxembourg', 'Latvia', 'North Macedonia', 'Malta', 'Netherlands',
+                   'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'Sweden', 'Slovenia',
+                   'Slovakia', 'Ukraine', 'Bosnia and Herzegovina',
+                   'Croatia', 'Moldova', 'Monaco', 'Montenegro', 'Serbia', 'Spain', 'Switzerland',
+                   'United Kingdom']
+        return sorted(country)
+
+    def getAsiaCountries(self):
+        country = ['Afghanistan', 'Armenia', 'Azerbaijan', 'Bangladesh', 'Bahrain', 'Brunei',
+                   'Bhutan', 'China', 'Cyprus', 'Georgia', 'Indonesia', 'Israel',
+                   'India', 'Iraq', 'Iran', 'Jordan', 'Japan', 'Kyrgyzstan', 'Korea, South',
+                   'Kuwait', 'Lebanon', 'Mongolia', 'Maldives', 'Malaysia', 'Nepal', 'Oman',
+                   'Philippines', 'Pakistan', 'Qatar', 'Saudi Arabia', 'Singapore', 'Syria', 'Thailand',
+                   'Tajikistan', 'Turkey', 'Uzbekistan', 'Vietnam', 'Yemen', 'Cambodia',
+                   'Timor-Leste', 'Kazakhstan', 'Laos', 'Sri Lanka', 'United Arab Emirates']
+        return sorted(country)
+
+    def getNorthAmericaCountries(self):
+        country = ['Antigua and Barbuda', 'Barbados', 'Bahamas', 'Belize', 'Canada', 'Costa Rica', 'Cuba',
+                   'Dominica', 'Dominican Republic', 'Guatemala', 'Haiti', 'Honduras', 'Jamaica', 'Mexico',
+                   'Nicaragua', 'Panama', 'Trinidad and Tobago', 'US', 'El Salvador', 'Grenada',
+                   'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines']
+        return sorted(country)
+
+    def getSouthAmericaCountries(self):
+        country = ['Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Guyana', 'Peru',
+                   'Paraguay', 'Suriname', 'Uruguay', 'Venezuela']
+        return sorted(country)
+
+    def getAfricaCountries(self):
+        country = ['Angola', 'Burkina Faso', 'Burundi', 'Benin', 'Botswana',
+                   'Congo (Brazzaville)', 'Congo (Kinshasa)', 'Cote d\'Ivoire', 'Cameroon', 'Cabo Verde', 'Djibouti', 'Egypt',
+                   'Eritrea', 'Ethiopia', 'Gabon', 'Ghana', 'Gambia', 'Guinea', 'Guinea-Bissau', 'Kenya',
+                   'Liberia', 'Lesotho', 'Libya', 'Madagascar', 'Mali', 'Mauritania', 'Mauritius', 'Malawi', 'Mozambique',
+                   'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Seychelles', 'Sudan', 'Sierra Leone', 'Senegal', 'Somalia',
+                   'Togo', 'Tunisia', 'Tanzania', 'Uganda', 'Zambia', 'Zimbabwe', 'Algeria', 'Central African Republic',
+                   'Chad', 'Comoros', 'Equatorial Guinea', 'Morocco', 'South Africa']
+        return sorted(country)
