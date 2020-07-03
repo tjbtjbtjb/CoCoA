8d94bb6dfb298ed03bd59675d1a5cbf94eed9e01
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2020-07-03 12:27:10 +0200

    with new inheritances of errors

--- cocoa/error.py
+++ cocoa/error.py
@@ -1,18 +1,33 @@
+# -*- coding: utf-8 -*-
+""" Project : CoCoA
+Date :    april-july 2020
+Authors : Olivier Dadoun, Julien Browaeys, Tristan Beau
+Copyright © CoCoa-team-17
+License: See joint LICENSE file
+
+Module : cocoa.error
+About : 
+
+Main class definitions for error management within the cocoa framework.
+All Cocoa exceptions should derive from the main CocoaError class.
+"""
 
 class CocoaError(Exception):
     """Base class for exceptions in CoCoa."""
-    pass
+    def __init__(self, message):
+        self.message = message
+        Exception(message)
 
-    
-class CocoaKeyError(CocoaError):
+class CocoaKeyError(CocoaError, KeyError):
     """Exception raised for errors in used key option.
 
     Attributes:
         message -- explanation of the error
     """
-
     def __init__(self, message):
         self.message = message
+        KeyError(message)
+        CocoaError(message)
 
 class CocoaDbError(CocoaError):
     """Exception raised for database errors.
@@ -20,16 +35,28 @@ class CocoaDbError(CocoaError):
     Attributes:
         message -- explanation of the error
     """
-
     def __init__(self, message):
         self.message = message
-
-class CocoaWhereError(CocoaError):
+        CocoaError(message)
+        
+class CocoaWhereError(CocoaError, IndexError):
     """Exception raised for location errors.
 
     Attributes:
         message -- explanation of the error
     """
-
     def __init__(self, message):
         self.message = message
+        IndexError(message)
+        CocoaError(message)
+        
+class CocoaTypeError(CocoaError, TypeError):
+    """Exception raised for type mismatch errors.
+
+    Attributes:
+        message -- explanation of the error
+    """
+    def __init__(self, message):
+        self.message = message
+        TypeError(message)
+        CocoaError(message)    
