153825a7cec86b3fd3c3fe2186abf50f2999df99
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2020-07-04 13:42:05 +0200

    translation of names to standard names for JHU database. Tested : ok.

--- cocoa/geo.py
+++ cocoa/geo.py
@@ -19,17 +19,27 @@ import warnings
 
 class GeoManager():
     
-    _list_standard=['iso2',    # Iso2 standard, default
+    _list_standard=['iso2',   # Iso2 standard, default
             'iso3',           # Iso3 standard
             'name',           # Standard name ( != Official, caution )
             'num']            # Numeric standard
-    _standard = None
+            
+    _list_db=[None,'JHU','worldometer'] # first is default
+    _list_output=['list','dict','pandas'] # first is default
+    
+    _standard = None # currently used normalisation standard
     
     def __init__(self,standard=_list_standard[0]):
         self.set_standard(standard)
     
     def get_list_standard(self):
         return self._list_standard
+        
+    def get_list_output(self):
+        return self._list_output
+        
+    def get_list_db(self):
+        return self._list_db
         
     def get_standard(self):
         return self._standard
@@ -48,7 +58,7 @@ class GeoManager():
     def get_standard(self):
         return self._standard
     
-    def to_standard(self, w, output='list'):
+    def to_standard(self, w, **kwargs):
         """Given a list of string of locations (countries), returns a
         normalised list according to the used standard (defined
         via the setStandard() or __init__ function. Current default is iso2.
@@ -58,14 +68,31 @@ class GeoManager():
         first arg  --   w, list of string of locations (or single string)
                         to convert to standard one
         
-        output    --    'list' (default), 'dict' or 'pandas' 
-        
+        output     --   'list' (default), 'dict' or 'pandas' 
+        db         --   database name to help conversion. 
+                        Default : None, meaning best effort to convert.
+                        Known database : JHU, wordometer
         """
+        
+        output=kwargs.get('output',self.get_list_output()[0])
+        if output not in self.get_list_output():
+            raise CocoaKeyError('Incorrect output type. See get_list_output()'
+                ' or help.')
+            
+        db=kwargs.get('db',self.get_list_db()[0])
+        if db not in self.get_list_db():
+            raise CocoaDbError('Unknown database for translation to '
+                'standardized location names. See get_list_db() or help.')
+        
+        w0=w
         if isinstance(w,str):
             w=[w]
         elif not isinstance(w,list):
             raise CocoaTypeError('Waiting for str, list of str or pandas'
                 'as input of get_standard function member of GeoManager')
+        
+        if db:
+            w=self.first_db_translation(w,db)
                 
         n=[] # will contain standardized name of countries (if possible)
         
@@ -75,44 +102,63 @@ class GeoManager():
             elif type(c)!=str:
                 raise CocoaTypeError('Locations should be given as '
                     'strings or integers only')
-                    
-            try:
-                n0=pc.countries.lookup(c)
-            except LookupError:
-                try:
-                    nf=pc.countries.search_fuzzy(c)
-                    if len(nf)>1:
-                        warnings.warn('Caution. More than one country match the key "'+\
-                        c+'" : '+str([ (k.name+', ') for k in nf])+\
-                        ', using first one.\n')
-                    n0=nf[0]
-                except LookupError:
-                    raise CocoaLookupError('No country match the key "'+c+'". Error.')
-                except Exception as e1:
-                    raise CocoaNotManagedError('Not managed error '+type(e1))
-            except Exception as e2:
-                raise CocoaNotManagedError('Not managed error'+type(e1))
             
-            if self._standard=='iso2':
-                n1=n0.alpha_2
-            elif self._standard=='iso3':
-                n1=n0.alpha_3
-            elif self._standard=='name':
-                n1=n0.name
-            elif self._standard=='num':
-                n1=n0.numeric
+            if len(c)==0:
+                n1=None
             else:
-                raise CocoaKeyError('Current standard is '+self._standard+\
-                    ' which is not managed. Error.')
+                try:
+                    n0=pc.countries.lookup(c)
+                except LookupError:
+                    try:
+                        nf=pc.countries.search_fuzzy(c)
+                        if len(nf)>1:
+                            warnings.warn('Caution. More than one country match the key "'+\
+                            c+'" : '+str([ (k.name+', ') for k in nf])+\
+                            ', using first one.\n')
+                        n0=nf[0]
+                    except LookupError:
+                        raise CocoaLookupError('No country match the key "'+c+'". Error.')
+                    except Exception as e1:
+                        raise CocoaNotManagedError('Not managed error '+type(e1))
+                except Exception as e2:
+                    raise CocoaNotManagedError('Not managed error'+type(e1))
+                
+                if self._standard=='iso2':
+                    n1=n0.alpha_2
+                elif self._standard=='iso3':
+                    n1=n0.alpha_3
+                elif self._standard=='name':
+                    n1=n0.name
+                elif self._standard=='num':
+                    n1=n0.numeric
+                else:
+                    raise CocoaKeyError('Current standard is '+self._standard+\
+                        ' which is not managed. Error.')
                     
             n.append(n1)
             
         if output=='list':
             return n
         elif output=='dict':
-            return dict(zip(w, n))
+            return dict(zip(w0, n))
         elif output=='pandas':
             return pd.DataFrame({'inputname':w,self._standard:n})
         else:
-            raise CocoaKeyError('Output should be "list", "dict" or "pandas"'+\
-                ' only, whereas "'+output+'" is given.')
+            return None # should not be here
+            
+    def first_db_translation(self,w,db):
+        if db=='JHU':
+            translation_dict={\
+                "Congo (Brazzaville)":"Republic of the Congo",\
+                "Congo (Kinshasa)":"COD",\
+                "Korea, South":"KOR",\
+                "Taiwan*":"Taiwan",\
+                "Laos":"LAO",\
+                "West Bank and Gaza":"PSE",\
+                "Burma":"Myanmar",\
+                "Iran":"IRN",\
+                "Diamond Princess":"",\
+                "MS Zaandam":""           
+                    }  # last two are names of boats
+                    
+        return [translation_dict.get(k,k) for k in w]
