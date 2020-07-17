496c5c7cc24860afb4277333d3cde243449a4e75
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2020-07-17 15:39:52 +0200

    bug solved with region_list(duplicates of rows)

--- cocoa/geo.py
+++ cocoa/geo.py
@@ -287,7 +287,7 @@ class GeoInfo():
         """ __init__ member function.
         """
         self._gm=GeoManager()
-        self._gr=GeoRegion()
+        self._grp=GeoRegion().get_pandas()
 
     def get_list_field(self):
         """ return the list of supported additionnal fields available
@@ -324,11 +324,12 @@ class GeoInfo():
         """
 
         # --- kwargs analysis ---
-        p=kwargs.get('input',None).copy() # the panda
+        p=kwargs.get('input',None) # the panda
         if not isinstance(p,pd.DataFrame):
             raise CocoaTypeError('You should provide a valid input pandas'
                 ' DataFrame as input. See help.')
-
+        p=p.copy()
+        
         overload=kwargs.get('overload',False)
         if not isinstance(overload,bool):
             raise CocoaTypeError('The overload option should be a boolean.')
@@ -372,7 +373,7 @@ class GeoInfo():
             # ----------------------------------------------------------
             elif f == 'continent_name':
                 p[f] = [pcc.convert_continent_code_to_continent_name( \
-                    pcc.country_alpha2_to_continent_code(k)) for k in countries_iso2]
+                    pcc.country_alpha2_to_continent_code(k) ) for k in countries_iso2 ]
             # ----------------------------------------------------------
             elif f == 'country_name':
                 p[f] = [pcc.country_alpha2_to_country_name(k) for k in countries_iso2]
@@ -416,10 +417,23 @@ class GeoInfo():
                         left_on='iso3_tmp',right_on='iso3_tmp2',\
                         suffixes=('','_tmp')).drop(['iso3_tmp2'],axis=1)
             # ----------------------------------------------------------
-            elif f in ['region_code_list','region_name_list','capital']:
-                p=p.merge(self._gr.get_pandas()[['iso3',f]],how='left',\
+            elif f in ['region_code_list','region_name_list']:
+                
+                if f == 'region_code_list':
+                    ff = 'region'
+                elif f == 'region_name_list':
+                    ff = 'region_name'
+                    
+                p[f]=p.merge(self._grp[['iso3',ff]],how='left',\
                     left_on='iso3_tmp',right_on='iso3',\
-                    suffixes=('','_tmp')).drop(['iso3'],axis=1)
+                    suffixes=('','_tmp')) \
+                    .groupby('iso3_tmp')[ff].apply(list).to_list()
+            # ----------------------------------------------------------    
+            elif f in ['capital']:
+                p[f]=p.merge(self._grp[['iso3',f]].drop_duplicates(), \
+                    how='left',left_on='iso3_tmp',right_on='iso3',\
+                    suffixes=('','_tmp'))[f]
+                
             # ----------------------------------------------------------
             elif f == 'geometry':
                 if self._data_geometry.empty:
@@ -437,7 +451,7 @@ class GeoInfo():
                     left_on='iso3_tmp',right_on='id_tmp',\
                     suffixes=('','_tmp')).drop(['id_tmp'],axis=1)
 
-        return p.drop(['iso2_tmp','iso3_tmp'],axis=1)
+        return p.drop(['iso2_tmp','iso3_tmp'],axis=1,errors='ignore')
 
 # ---------------------------------------------------------------------
 # --- GeoInfo class ---------------------------------------------------
