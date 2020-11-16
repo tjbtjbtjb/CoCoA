9ebf74d5e0490a63dbf265d587d9f21fe38e31bf
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2020-11-16 14:15:57 +0100

    versionning attempt #3

--- __init__.py
+++ __init__.py
@@ -1,3 +1,1 @@
-from _version import __version__
-__author__ = 'Tristan Beau, Julien Browaeys, Olivier Dadoun'
-__email__ = 'cocoa@framalistes.org'
+from cocoa._version import __version__,__author__,__email__
